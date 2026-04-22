22f055f5493e40c62379829e2d958aae29a6d6d6
nK1SS <nathankissinger04@gmail.com>
2026-04-22 14:04:00 -0500

    Add files via upload

---
+++ BajaCVTSim_RUN.m
@@ -0,0 +1,14 @@
+%File Name:        BajaCVTSim_RUN.m
+%Design Team:      F-25-Baja CVT
+%Author:           Nathan Kissinger
+%Last Edit Date:   03/29/2026
+
+%File Description: Only file user needs to run in the BajaCVTSim Folder to
+%                  begin simulation.
+
+%Clear Workspace
+clc
+clear
+
+%Load the Application Interface
+BajaCVTSim_App
